0160f8829aa2be7dcfe828f5c6f607a0ffb6b540
AEHimself <57598335+aehimself@users.noreply.github.com>
2022-12-24 15:00:26 +0100

    Do not link framework files, use Library path to access them

--- VStarCamDownloader.dpr
+++ VStarCamDownloader.dpr
@@ -7,11 +7,7 @@ uses
   uVStarCamDownloader in 'uVStarCamDownloader.pas',
   uDownloaderEngine in 'uDownloaderEngine.pas',
   uSettings in 'uSettings.pas',
-  AE.Application.Engine in '..\_DelphiComponents\AEFramework\AE.Application.Engine.pas',
-  AE.Application.Helper in '..\_DelphiComponents\AEFramework\AE.Application.Helper.pas',
-  AE.Application.Settings in '..\_DelphiComponents\AEFramework\AE.Application.Settings.pas',
-  AE.Application.Application in '..\_DelphiComponents\AEFramework\AE.Application.Application.pas',
-  AE.Application.Console in '..\_DelphiComponents\AEFramework\AE.Application.Console.pas';
+  AE.Application.Console;
 
 {$R *.RES}
 
@@ -21,7 +17,7 @@ Begin
    If Not Application.DelayInitialize Or Application.Installing Then
      Application.Initialize;
    Application.CreateForm(TVStarCamDownloaderService, VStarCamDownloaderService);
-   Application.Run;
+  Application.Run;
  End
  Else
    StartWithConsole(TVStarCamDownloader);
