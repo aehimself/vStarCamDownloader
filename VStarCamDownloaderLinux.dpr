0f854898f36b9fa810f31f34232f1b427bbbb954
AEHimself <57598335+aehimself@users.noreply.github.com>
2024-05-22 22:09:12 +0200

    VStarCam downloader Linux executable

---
+++ VStarCamDownloaderLinux.dpr
@@ -0,0 +1,32 @@
+﻿program VStarCamDownloaderLinux;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils,
+  uSettings in 'uSettings.pas',
+  uVStarCamDownloader in 'uVStarCamDownloader.pas',
+  uDownloaderEngine in 'uDownloaderEngine.pas';
+
+Type
+  TDummy = Class
+    Class Procedure ConsoleWrite(inText: String);
+  End;
+Var
+  vsdl: TVStarCamDownloader;
+
+Class Procedure TDummy.ConsoleWrite(inText: String);
+Begin
+  WriteLn(inText);
+End;
+
+Begin
+  vsdl := TVStarCamDownloader.Create(TDummy.ConsoleWrite);
+  Try
+    ReadLn;
+  Finally
+    FreeAndNil(vsdl);
+  End;
+End.
